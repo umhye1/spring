b6aef87c1141c4da2283c5a0a22788cad9992d9c
umhye1 <lucy021206@gmail.com>
2024-05-30 15:11:27 +0900

    Revert "입문-섹션6.1"
    This reverts commit c14c6b6d3e542fde53fad24dc6f804c68f6d3141.

--- hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -4,11 +4,8 @@ import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-
-import java.util.List;
 
 @Controller
 public class MemberController {
@@ -33,11 +30,8 @@ public class MemberController {
 
         return"redirect:/";
     }
-
-    @GetMapping("/members")
-    public String list(Model model){
-       List<Member> members = memberService.findMembers();
-        model.addAttribute("members",members);
-        return "members/memberList";
-    }
+//    @Autowired
+//    public MemberController(MemberService memberService){
+//        this.memberService= memberService;
+//    }
 }
