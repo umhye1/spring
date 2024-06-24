ed6027d07c2315e554ad4167e9425835a1d48e48
umhye1 <lucy021206@gmail.com>
2024-06-24 23:58:53 +0900

    spring 입문 - 6

--- hello-spring/src/main/java/hello/hello_spring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hello_spring/service/MemberService.java
@@ -21,10 +21,19 @@ public class MemberService {
     //회원가입
     public Long join(Member member){
 
-        // 같은 이름이 있는 중복회원은 안 됨
-        validateDuplicateMember(member); // 중복회원 검증
-        memberRepository.save(member);
-        return member.getId();
+        long start = System.currentTimeMillis();
+
+        try{
+            // 같은 이름이 있는 중복회원은 안 됨
+            validateDuplicateMember(member); // 중복회원 검증
+            memberRepository.save(member);
+            return member.getId();
+        }finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish-start;
+            System.out.println("join = "+timeMs + "ms");
+
+        }
     }
 
     private void validateDuplicateMember(Member member) {
