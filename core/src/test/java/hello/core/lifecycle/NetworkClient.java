fd6dcf51739705ccc49ae681da64ba09c3184d6a
umhye1 <lucy021206@gmail.com>
2024-10-24 17:34:29 +0900

    spring 핵심 원리 - 8.4 애노테이션 @PostConstruct, @PreDestroy

--- core/src/test/java/hello/core/lifecycle/NetworkClient.java
+++ core/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,5 +1,8 @@
 package hello.core.lifecycle;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
 //가짜 네트워크 클라이언트
 public class NetworkClient {
     private String url;
@@ -25,13 +28,15 @@ public class NetworkClient {
         System.out.println("close: "+url);
     }
 
-    //의존 관계 주입 후 연결
+
+    @PostConstruct
     public void init(){
         System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메시지");
     }
 
+    @PreDestroy
     public void close(){
         System.out.println("NetworkClient.close");
         disconnect();
