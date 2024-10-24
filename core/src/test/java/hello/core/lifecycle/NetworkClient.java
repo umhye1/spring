34f65a679681b05f1844fc79c4ab47519a2e40ee
umhye1 <lucy021206@gmail.com>
2024-10-24 16:52:07 +0900

    spring 핵심 원리 - 8.2 인터페이스 InitializingBean, DisposableBean

--- core/src/test/java/hello/core/lifecycle/NetworkClient.java
+++ core/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,7 +1,10 @@
 package hello.core.lifecycle;
 
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
 //가짜 네트워크 클라이언트
-public class NetworkClient {
+public class NetworkClient implements InitializingBean, DisposableBean { // 초기화 빈
     private String url;
     public NetworkClient(){
         System.out.println("생성자 호출 , url = " + url);
@@ -14,8 +17,6 @@ public class NetworkClient {
     //서비스 시작시 호출
     public void connect(){
         System.out.println("connect: "+url);
-        connect();
-        call("초기화 연결 메시지");
     }
 
     public void call(String message){
@@ -25,5 +26,17 @@ public class NetworkClient {
     //서비스 종료시 호출
     public void disconnect(){
         System.out.println("close: "+url);
+    }
+
+    //의존 관계 주입 후 연결
+    @Override
+    public void afterPropertiesSet()throws Exception{
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        disconnect();
     }
 }
