ab5d04982353170ffcaaf62c5a2b91c613fdb259
umhye1 <lucy021206@gmail.com>
2024-10-24 18:04:05 +0900

    spring 핵심 원리 - 9.1 싱글톤, 프로토타입 스코프

---
+++ core/src/test/java/hello/core/scope/PrototypeTest.java
@@ -0,0 +1,39 @@
+package hello.core.scope;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+public class PrototypeTest {
+    @Test
+    void prototypeBeanFind(){
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+        System.out.println("find prototypeBean1");
+        PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
+        System.out.println("find prototypeBean2");
+        PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
+        System.out.println("prototypeBean1 = " + prototypeBean1);
+        System.out.println("prototypeBean2 = " + prototypeBean2);
+        Assertions.assertThat(prototypeBean1).isNotSameAs(prototypeBean2);
+
+        ac.close();
+        //싱글톤 빈은 스프링 컨테이너 생성 시점에 초기화 메서드가 실행 되지만,
+        // 프로토타입 스코프의 빈은 스프링 컨테 이너에서 빈을 조회할 때 생성되고, 초기화 메서드도 실행된다.
+
+
+    }
+    @Scope("prototype")
+    static class PrototypeBean{
+        @PostConstruct
+        public void init(){
+            System.out.println("PrototypeBean.init");
+        }
+        @PreDestroy
+        public void destroy(){
+            System.out.println("PrototypeBean.destroy");
+        }
+    }
+}
