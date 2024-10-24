dd90868177b7cc7cb812b3fbe350341560a712fc
umhye1 <lucy021206@gmail.com>
2024-10-24 13:28:22 +0900

    spring 핵심 원리 -8.1 빈 생명주기 콜백 시작

---
+++ core/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -0,0 +1,29 @@
+package hello.core.lifecycle;
+
+//가짜 네트워크 클라이언트
+public class NetworkClient {
+    private String url;
+    public NetworkClient(){
+        System.out.println("생성자 호출 , url = " + url);
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    //서비스 시작시 호출
+    public void connect(){
+        System.out.println("connect: "+url);
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    public void call(String message){
+        System.out.println("call: " + url + " message = " + message);
+    }
+
+    //서비스 종료시 호출
+    public void disconnect(){
+        System.out.println("close: "+url);
+    }
+}
