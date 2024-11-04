c9bfd5b115ef79da77714ea7d68b3e4928990ad1
umhye1 <lucy021206@gmail.com>
2024-11-05 01:44:53 +0900

    jpa 활용 - 5.2 회원 서비스 개발

--- jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -33,7 +33,7 @@ public class MemberRepositoryTest {
         Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
         Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
         Assertions.assertThat(findMember).isEqualTo(member);
-        System.out.println("findMember == member " + (findMember == member));
+        System.out.println("findMember == member " + (findMember == member ));
     }
 
 }
