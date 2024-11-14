0ebe4b1c52f1a78a9b43d5ff240a8dff87d48658
umhye1 <lucy021206@gmail.com>
2024-11-14 17:13:06 +0900

    jpa 활용 - 7.3 주문 서비스 개발

--- jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -1,13 +1,16 @@
 package jpabook.jpashop.domain;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import static jakarta.persistence.FetchType.*;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderItem {
 
     @Id @GeneratedValue
@@ -24,6 +27,8 @@ public class OrderItem {
 
     private int orderPrice; //주문 가격
     private int count;
+
+    protected OrderItem(){}
 
 // 생성 메서드
     public static OrderItem createOrderItem(Item item,int orderPrice, int count){
