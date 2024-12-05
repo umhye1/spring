58990dc0fa4203294f0efa2d9ed78735db65b574
umhye1 <lucy021206@gmail.com>
2024-12-05 17:35:59 +0900

    jpa 기본 - 4.1 영속성 컨텍스트 1

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -15,11 +15,15 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try {
-//            Member findMember = em.find(Member.class, 2L);
-            List<Member> result = em.createQuery("select m from Member as m", Member.class).getResultList();
-            for(Member member : result){
-                System.out.println("member.name = " + member.getName());
-            }
+            //비영속
+            Member member = new Member();
+            member.setId(100L);
+            member.setName("HelloJPA");
+
+            //영속
+            System.out.println("=== BEFORE ===");
+            em.persist(member);
+            System.out.println("=== AFTER ===");
 
             tx.commit();
         }catch (Exception e){
