2e172e9ec7c9e07eca25e549d3d35a1518f7e98c
umhye1 <lucy021206@gmail.com>
2024-12-05 17:59:17 +0900

    jpa 기본 - 4.2-1 영속 엔티티의 동일성 보장

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -17,13 +17,26 @@ public class JpaMain {
         try {
             //비영속
             Member member = new Member();
-            member.setId(100L);
+            member.setId(101L);
             member.setName("HelloJPA");
 
             //영속
-            System.out.println("=== BEFORE ===");
-            em.persist(member);
-            System.out.println("=== AFTER ===");
+            Member findMember1 = em.find(Member.class, 101L);
+            Member findMember2 = em.find(Member.class, 101L);
+
+            //영속엔티티의 동일성 보장
+            System.out.println("result = "+ (findMember1 == findMember2));
+
+//            //영속
+//            System.out.println("=== BEFORE ===");
+//            em.persist(member); //1차 캐시에 저장됨
+//            System.out.println("=== AFTER ===");
+//
+//            Member findMember = em.find(Member.class, 101L);
+//            System.out.println("findMember.id = " + findMember.getId());
+//            System.out.println("findMember.name = " + findMember.getName());
+
+
 
             tx.commit();
         }catch (Exception e){
