2e52ca833724edf0f443ba4f26c3f95e92da9669
umhye1 <lucy021206@gmail.com>
2024-12-17 01:55:34 +0900

    jpa 기본 - 5.5 실전 예제 1 - 요구사항 분석과 기본 매핑

---
+++ ex1-hello-jpa1/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -0,0 +1,63 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class OrderItem {
+
+    @Id@GeneratedValue
+    @Column(name = "ORDER_ITEM_ID")
+    private Long id;
+
+    @Column(name = "ORDER_ID")
+    private Long orderId;
+
+    @Column(name = "ITEM_ID")
+    private Long ItemId;
+
+    private int OrderPrice;
+    private int count;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    public Long getItemId() {
+        return ItemId;
+    }
+
+    public void setItemId(Long itemId) {
+        ItemId = itemId;
+    }
+
+    public int getOrderPrice() {
+        return OrderPrice;
+    }
+
+    public void setOrderPrice(int orderPrice) {
+        OrderPrice = orderPrice;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+}
