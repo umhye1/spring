13a315952b2128971eb8ef8a8f1c3778bbd6950f
umhye1 <lucy021206@gmail.com>
2024-12-23 17:19:25 +0900

    jpa 기본 - 일대일 [1:1]

--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -15,12 +15,16 @@ public class Member {
     @Column(name = "MEMBER_ID")
     private Long id;
 
-   @Column(name = "USERNAME")
-   private String username;
+    @Column(name = "USERNAME")
+    private String username;
 
-   @ManyToOne
-   @JoinColumn(name  = "TEAM_ID" ,insertable = false, updatable = false) // 읽기 전용 필드
-   private Team team;
+    @ManyToOne
+    @JoinColumn(name  = "TEAM_ID" ,insertable = false, updatable = false) // 읽기 전용 필드
+    private Team team;
+
+    @OneToOne
+    @JoinColumn(name = "Locker_ID")
+    private Locker locker;
 
 
     public Long getId() {   return id;  }
