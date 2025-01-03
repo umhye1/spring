0e4921e7ce4d9f070f802cb7ee7915201e6bf5f0
umhye1 <lucy021206@gmail.com>
2025-01-03 19:28:12 +0900

    jpa 기본 - 10.2 임베디드 타입

--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -1,6 +1,7 @@
 package hellojpa;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
@@ -14,26 +15,51 @@ public class Member extends BaseEntity{
     @Column(name = "USERNAME")
     private String username;
 
-    @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn
-    private Team team;
+    //period
+    @Embedded //생략 가능
+    private Period workPeriod;
 
+    //address
+    @Embedded //생략 가능
+    private Address homeAddress;
 
-    public Long getId() {   return id;  }
+    @Embedded //생략 가능
+    @AttributeOverrides({ // 컬럼 명 속성 재정의
+            @AttributeOverride(name = "city", column = @Column(name ="WORK_CITY")),
+            @AttributeOverride(name = "street", column = @Column(name ="WORK_STREET")),
+            @AttributeOverride(name = "zipcode", column = @Column(name ="WORK_ZIPCODE"))
+    })
+    private Address workAddress;
 
-    public void setId(Long id) {    this.id = id;   }
-
-    public String getUsername() {   return username;    }
-
-    public void setUsername(String username) {  this.username = username;   }
-
-    public Team getTeam() {
-        return team;
+    public Long getId() {
+        return id;
     }
 
-    public void setTeam(Team team) {
-        this.team = team;
+    public void setId(Long id) {
+        this.id = id;
     }
 
+    public String getUsername() {
+        return username;
+    }
 
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Period getWorkPeriod() {
+        return workPeriod;
+    }
+
+    public void setWorkPeriod(Period workPeriod) {
+        this.workPeriod = workPeriod;
+    }
+
+    public Address getHomeAddress() {
+        return homeAddress;
+    }
+
+    public void setHomeAddress(Address homeAddress) {
+        this.homeAddress = homeAddress;
+    }
 }
