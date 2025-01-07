374e37078c4c2e51249bd6c75f51445ee3fe4522
umhye1 <lucy021206@gmail.com>
2025-01-07 17:56:47 +0900

    jpa 기본 -10.6  실전 예제 6 - 값 타입 매핑

--- ex1-hello-jpa1/src/main/java/jpabook/jpashop/domain/Delivery.java
+++ ex1-hello-jpa1/src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -13,9 +13,40 @@ public class Delivery extends BaseEntity {
     @OneToOne(mappedBy = "delivery", fetch = LAZY)
     private Order order;
 
-    private String city;
-    private String street;
-    private String zipcode;
-
     private DeliveryStatus deliveryStatus;
+
+    @Embedded
+    private Address address;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public DeliveryStatus getDeliveryStatus() {
+        return deliveryStatus;
+    }
+
+    public void setDeliveryStatus(DeliveryStatus deliveryStatus) {
+        this.deliveryStatus = deliveryStatus;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
 }
