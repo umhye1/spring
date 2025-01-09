fb616160286ae5b2553efaec9dc661ac15866463
umhye1 <lucy021206@gmail.com>
2025-01-09 18:39:39 +0900

    jpa 기본 - 11. 3 프로젝션(SELECT)

--- jpql/src/main/java/jpql/JpaMain.java
+++ jpql/src/main/java/jpql/JpaMain.java
@@ -15,25 +15,40 @@ public class JpaMain {
             member.setAge(10);
             em.persist(member);
 
-            //반환 타입이 명확 -> TypeQuery
-            TypedQuery<Member> query = em.createQuery("select m from Member m", Member.class);
-            TypedQuery<String> query2 = em.createQuery("select m.username from Member m", String.class);
+            em.flush();
+            em.clear();
 
-            // 반환타입 명확하지 않을 때 query
-            Query query3 = em.createQuery("select m.username, m.age from Member m");
+            // 엔티티 프로젝션
+            List<Team> result = em.createQuery("select t from Member m join m.team t", Team.class)
+                    .getResultList();
 
-            // 결과조회 - getResultList()
-            List<Member> resultList = query.getResultList();
-            for (Member member1 : resultList) {
-                System.out.println("member1 = " + member1);
-            }
+            //임베디드 프로젝션
+            em.createQuery("select o.address from Order o", Address.class)
+                    .getResultList();
 
-            // getSingleResult()
-            Member result = em.createQuery("select m from Member m where m.username =: username", Member.class)
-                    .setParameter("username", "member1")// 반환타입 명확하지 않을 때 query
-                    .getSingleResult();
-            System.out.println("singleResult.getUsername() = " + result.getUsername());
+            //스칼라 타입 프로젝션
+            List resultList = em.createQuery("select m.username, m.age from Member m")
+                    .getResultList();
 
+            //프로젝션 - 여러 값 조회 : 2. object[] 타입으로 조회
+            Object o = resultList.get(0);
+            // 타입캐스팅
+            Object[] result1 = (Object[]) o;
+            System.out.println("username = " + result1[0]);
+            System.out.println("age = " + result1[1]);
+
+
+            //프로젝션 - 여러 값 조회 : 타입캐스팅 없이
+            List<Object[]> resultList2 = em.createQuery("select m.username, m.age from Member m")
+                    .getResultList();
+
+            //프로젝션 - 여러 값 조회 : 3. new
+            List<MemberDTO> resultList3 = em.createQuery("select new jpql.MemberDTO(m.username, m.age)from Member m", MemberDTO.class)
+                    .getResultList();
+
+            MemberDTO memberDTO = resultList3.get(0);
+            System.out.println("memberDTO.getUsername() = " + memberDTO.getUsername());
+            System.out.println("memberDTO = " + memberDTO.getAge());
 
             tx.commit();
         }catch (Exception e){
