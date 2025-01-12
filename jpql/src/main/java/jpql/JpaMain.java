f4b93166dc389f6861c8ff891eb0acce83e64397
umhye1 <lucy021206@gmail.com>
2025-01-13 00:03:02 +0900

    jpa 기본 - 11.9 JPQL 함수

--- jpql/src/main/java/jpql/JpaMain.java
+++ jpql/src/main/java/jpql/JpaMain.java
@@ -10,35 +10,31 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try {
+
             Team team = new Team();
-            team.setName("teamA");
             em.persist(team);
 
             Member member = new Member();
             member.setUsername("관리자");
-            member.setAge(10);
-            member.setType(MemberType.ADMIN);
-
             member.setTeam(team);
 
             em.persist(member);
 
+            Member member2 = new Member();
+            member2.setUsername("관리자2");
+
+            em.persist(member2);
+
             em.flush();
             em.clear();
 
-//            String query = "select "+
-//                    "    case when m.age <= 10 then '학생요금'"+
-//                    "         when m.age >= 60 then '경로요금'"+
-//                    "         else '일반요금' "+
-//                    "end "+
-//                    "from Member m";
-//            String query = "select coalesce(m.username, '이름 없는 회원') from Member m ";
+//            String query = "select concat('a','b') from Member m";
+//            String query = "select substring(m.username,2,3) from Member m";
+//            String query = "select locate('de','abcdefg') from Member m";
+            String query = "select size(t.members) From Team t";
+            List<Integer> result = em.createQuery(query, Integer.class).getResultList();
 
-            String query = "select nullif(m.username, '관리자') as username "+
-                    "from Member m";
-            List<String> result = em.createQuery(query, String.class).getResultList();
-
-            for (String s : result) {
+            for (Integer s : result) {
                 System.out.println("s = " + s);
             }
 
