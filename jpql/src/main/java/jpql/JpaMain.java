f0ba4b9f8aca3ac150482b01bf153c5745f2fe31
umhye1 <lucy021206@gmail.com>
2025-01-13 19:07:08 +0900

    jpa 기본 - 12.1 경로 표현식

--- jpql/src/main/java/jpql/JpaMain.java
+++ jpql/src/main/java/jpql/JpaMain.java
@@ -1,6 +1,7 @@
 package jpql;
 
 import javax.persistence.*;
+import java.util.Collection;
 import java.util.List;
 
 public class JpaMain {
@@ -15,28 +16,29 @@ public class JpaMain {
             em.persist(team);
 
             Member member = new Member();
-            member.setUsername("관리자");
+            member.setUsername("관리자1");
             member.setTeam(team);
-
             em.persist(member);
 
             Member member2 = new Member();
             member2.setUsername("관리자2");
-
+            member2.setTeam(team);
             em.persist(member2);
 
             em.flush();
             em.clear();
+// 묵시적 inner join
+//            String query = "select m.team From Member m";
+//            List<Team> result = em.createQuery(query, Team.class).getResultList();
 
-//            String query = "select concat('a','b') from Member m";
-//            String query = "select substring(m.username,2,3) from Member m";
-//            String query = "select locate('de','abcdefg') from Member m";
-            String query = "select size(t.members) From Team t";
-            List<Integer> result = em.createQuery(query, Integer.class).getResultList();
+//            String query = "select t.members.size From Team t";
+//            Integer result = em.createQuery(query, Integer.class).getSingleResult();
 
-            for (Integer s : result) {
-                System.out.println("s = " + s);
-            }
+
+            String query = "select m From t join t.members m";
+            List<Collection> result = em.createQuery(query, Collection.class).getResultList();
+
+            System.out.println("result = " + result);
 
             tx.commit();
         }catch (Exception e){
